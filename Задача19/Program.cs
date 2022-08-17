d4635982ab1405451e82cf90e7722bff9bf14761
Oksana bondarenko <roksanapopova@gmail.com>
2022-08-17 22:59:54 +0200

    Task 19 & ignor

---
+++ Задача19/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+bool Palindrome(int num)
+{
+    int a = num / 10000;
+    int b = num % 10;
+    bool result = false;
+
+    if (a == b)
+    {
+        a = num / 1000 % 10;
+        b = num / 10 % 10;
+        if (a == b)
+        {
+            result = true;
+        }
+    }
+    return result;
+
+} 
+
+int number = ReadInt("Введите пятизначное число: ");
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write(Palindrome(number));
